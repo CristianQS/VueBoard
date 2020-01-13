a5b82d4aa9730f0d633257a46c8da9ee00837a3d
CrisQ <crisitan_31teror@hotmail.com>
2020-01-13 21:26:56 +0000

    created BoardsContainer

---
+++ src/components/BoardsContainer.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div>
+        <div>{{hello}}</div>
+        <Board/>
+    </div>
+</template>
+
+<script lang="ts">
+import { createComponent, ref} from "@vue/composition-api";
+import { Component, Vue } from 'vue-property-decorator';
+import BoardVue from "./Board.vue";
+
+
+export default  createComponent({
+    components: {
+        Board: BoardVue
+    },
+    setup() {
+        const hello = ref("Hello")
+        return { hello }
+    }
+
+})
+</script>
+
+<style>
+
+</style>
