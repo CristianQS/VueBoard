5e4a7ec495144afca9d5fc7ced0a73b294008918
CrisQ <crisitan_31teror@hotmail.com>
2020-01-13 21:26:09 +0000

    install tailwind

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import VueCompositionApi from '@vue/composition-api' 
 import VueRX from 'vue-rx' 
+import './assets/index.css';
+
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi) 
 Vue.use(VueRX) 
+
 
 new Vue({
   router,
